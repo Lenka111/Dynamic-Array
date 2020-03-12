2db3b2b4433f1afbf65d98783284ad8558e87e0a
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-03-11 23:21:34 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,28 @@
 //Elena Voinu
 public class Main {
     public static void main(String[] args){
-    DynamicArray array = new DynamicArray(5);
+        
+        //create object of the DynamicArray class
+        DynamicArray array = new DynamicArray(5);
+        
+        //displaying the initial capacity of the array
         System.out.println("Initial array capacity is "+array.getInitialCapacity());
+        
+         //displaying how many elements are in the array
         System.out.println("Elements in the array: "+array.getSize());
 
+        //adding elements in the array, one at a time
         array.addElement(1);
         array.addElement(2);
         array.addElement(3);
         array.addElement(4);
         array.addElement(5);
         array.addElement(6);
+        
+        //display the array with all the added elemnts
         array.displayArray();
+        
+         //displaying how many elements are in the array
         System.out.println("Elements in the array "+array.getSize());
 
     }
